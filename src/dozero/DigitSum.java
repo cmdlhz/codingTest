7a1ef996485aea7aa741cdb58d6aa0feeaf35935
Administrator <Administrator@DESKTOP-5GLK0U8>
2019-11-01 01:39:06 +0900

    g

---
+++ src/dozero/DigitSum.java
@@ -0,0 +1,17 @@
+package dozero;
+
+public class DigitSum {
+	public int solution(int n) {
+		 int answer = 0;
+		String str = Integer.toString(n);
+		for(int i = 0; i<=str.length();i++) {
+			try {
+			answer += Integer.parseInt(str.substring(i,i+1));
+			}
+			catch(Exception e) {
+				return answer;
+			}
+		}
+		return answer;       
+    }
+}
